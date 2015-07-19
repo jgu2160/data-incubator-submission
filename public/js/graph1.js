f9249ee185e4791a051b53b5335d8cb41379b49e
jgu <jeffwgu@gmail.com>
2015-07-19 10:29:05 -0600

    completed graphs

--- public/js/graph1.js
+++ public/js/graph1.js
@@ -1,4 +1,4 @@
-var margin = {top: 20, right: 20, bottom: 50, left: 40},
+var margin = {top: 20, right: 20, bottom: 50, left: 80},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
@@ -48,7 +48,7 @@ var svg = d3.select(".graph1").append("svg")
       .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("Average Unigram (n=10)");
+      .text("Unigram % in 2006");
 
   svg.selectAll(".bar")
       .data(data)
---
+++ public/js/graph2.js
@@ -0,0 +1,62 @@
+var data = [[4593, 0.0022], [6382, 0.000012], [4742,0.00018]];
+
+    var margin = {top: 20, right: 15, bottom: 60, left: 60}
+      , width = 960 - margin.left - margin.right
+      , height = 500 - margin.top - margin.bottom;
+
+    var x = d3.scale.linear()
+              .domain([0, d3.max(data, function(d) { return d[0]; })])
+              .range([ 0, width ]);
+
+    var y = d3.scale.linear()
+    	      .domain([0, d3.max(data, function(d) { return d[1]; })])
+    	      .range([ height, 0 ]);
+
+    var chart = d3.select('.graph2')
+	.append('svg:svg')
+	.attr('width', width + margin.right + margin.left)
+	.attr('height', height + margin.top + margin.bottom)
+	.attr('class', 'chart');
+
+    var main = chart.append('g')
+	.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+	.attr('width', width)
+	.attr('height', height)
+	.attr('class', 'main');
+
+    // draw the x axis
+    var xAxis = d3.svg.axis()
+	.scale(x)
+	.orient('bottom');
+
+    main.append('g')
+	.attr('transform', 'translate(0,' + height + ')')
+	.attr('class', 'main axis date')
+	.call(xAxis);
+
+    // draw the y axis
+    var yAxis = d3.svg.axis()
+	.scale(y)
+	.orient('left')
+    .ticks(10, "%");
+
+    main.append('g')
+	.attr('transform', 'translate(0,0)')
+	.attr('class', 'main axis date')
+	.call(yAxis)
+    .append("text")
+      .attr("y", 450)
+      .attr("x", 525)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("(Lower is better)");
+
+
+    var g = main.append("svg:g");
+
+    g.selectAll("scatter-dots")
+      .data(data)
+      .enter().append("svg:circle")
+          .attr("cx", function (d,i) { return x(d[0]); } )
+          .attr("cy", function (d) { return y(d[1]); } )
+          .attr("r", 8);
